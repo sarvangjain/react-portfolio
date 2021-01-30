ef7a19eba51416d3ae08f13aa3b614327c34da3a
sarvangjain <18ucs062@lnmiit.ac.in>
2021-01-30 17:43:14 +0530

    Initial Commit

---
+++ src/portfolio.js
@@ -0,0 +1,380 @@
+﻿/* Change this file to get your personal Portfolio */
+
+// Summary And Greeting Section
+
+import emoji from 'react-easy-emoji';
+
+const illustration = {
+  animated: true, // set to false to use static SVG
+};
+
+const greeting = {
+  username: 'Sarvang Jain',
+  title: "Hello, I'm Sarvang Jain",
+  subTitle: emoji(
+    'A passionate and ingenious Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs and some other cool libraries and frameworks.'
+  ),
+  resumeLink:
+    '',
+  displayGreeting: true, // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: 'https://github.com/sarvangjain',
+  linkedin: 'https://www.linkedin.com/in/sarvang-jain-1207bb173/',
+  gmail: 'sarvangjain@gmail.com',
+  gitlab: 'https://gitlab.com/sarvangjain',
+  facebook: 'https://www.facebook.com/sarvang.jain.521/',
+  //medium: 'https://medium.com/@saadpasta',
+  //stackoverflow: 'https://stackoverflow.com/users/10422806/saad-pasta',
+  // Instagram and Twitter are also supported in the links!
+  display: true, // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: 'What I have done',
+  subTitle: 'CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK',
+  skills: [
+    emoji(
+      '⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications'
+    ),
+    emoji('⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks'),
+    emoji(
+      '⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean'
+    ),
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: 'JavaScript',
+      fontAwesomeClassname: 'fab fa-js',
+    },
+    {
+      skillName: 'reactjs',
+      fontAwesomeClassname: 'fab fa-react',
+    },
+    {
+      skillName: 'nodejs',
+      fontAwesomeClassname: 'fab fa-node',
+    },
+    /* {
+      skillName: 'swift',
+      fontAwesomeClassname: 'fab fa-swift',
+    }, */
+    {
+      skillName: 'npm',
+      fontAwesomeClassname: 'fab fa-npm',
+    },
+    {
+      skillName: 'html-5',
+      fontAwesomeClassname: 'fab fa-html5',
+    },
+    {
+      skillName: 'css3',
+      fontAwesomeClassname: 'fab fa-css3-alt',
+    },
+    {
+      skillName: 'sass',
+      fontAwesomeClassname: 'fab fa-sass',
+    },
+    {
+      skillName: 'sql-database',
+      fontAwesomeClassname: 'fas fa-database',
+    },
+    {
+      skillName: 'aws',
+      fontAwesomeClassname: 'fab fa-aws',
+    },
+    {
+      skillName: 'firebase',
+      fontAwesomeClassname: 'fas fa-fire',
+    },
+    {
+      skillName: 'python',
+      fontAwesomeClassname: 'fab fa-python',
+    },
+    {
+      skillName: 'docker',
+      fontAwesomeClassname: 'fab fa-docker',
+    },
+  ],
+  display: true, // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: 'The LNM Institute of Information Technology',
+      logo: require('./assets/images/lnm1.png'),
+      subHeader: 'Bachelor of Technology in Computer Science',
+      duration: 'August 2018 - Current',
+      desc: 'CGPA: 7.82 / 10.00',
+      /* descBullets: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      ], */
+    },
+    {
+      schoolName: 'Neerja Modi School',
+      logo: require('./assets/images/nms.png'),
+      subHeader: 'Class XII',
+      duration: 'April 2017 - May 2018',
+      desc: 'Percentage: 93.4%',
+      /* descBullets: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      ], */
+    },
+    {
+      schoolName: 'Neerja Modi School',
+      logo: require('./assets/images/nms.png'),
+      subHeader: 'Class X',
+      duration: 'April 2015 - May 2016',
+      desc: 'CGPA: 10/10',
+      /* descBullets: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      ], */
+    },
+  ],
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: 'Frontend/Design', //Insert stack or technology you have experience in
+      progressPercentage: '90%', //Insert relative proficiency in percentage
+    },
+    {
+      Stack: 'Programming',
+      progressPercentage: '80%',
+    },
+    {
+      Stack: 'Backend',
+      progressPercentage: '60%',
+    },
+  ],
+  displayCodersrank: false, // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: 'Software Developer',
+      company: 'Dhanu Infotech',
+      companylogo: require('./assets/images/dhanu3.png'),
+      date: 'Dec 2020 – Present',
+      desc:
+        'Working as a full-stack developer right now.',
+      descBullets: [
+        'At present, working on another project.',
+        'Developed an E-Commerce Website currently in Testing Phase',
+      ],
+    },
+    {
+      role: 'Front-End ReactJS Developer',
+      company: `Let's Develop It`,
+      companylogo: require('./assets/images/ldi.jpeg'),
+      date: 'Nov 2020 – Dec 2020',
+      desc:
+        'Developed an Admin Panel for a website and integrated the fornt-end to the backend of the website.',
+    },
+    /* {
+      role: 'Software Engineer Intern',
+      company: 'Airbnb',
+      companylogo: require('./assets/images/airbnbLogo.png'),
+      date: 'Jan 2015 – Sep 2015',
+      desc:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    }, */
+  ],
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: 'saadpasta', // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: 'true', // Set true or false to show Contact profile using Github, defaults to true
+  display: true, // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: 'Projects',
+  subtitle: 'SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH',
+  projects: [
+    {
+      image: require('./assets/images/saayaHealthLogo.webp'),
+      link: 'http://saayahealth.com/',
+    },
+    {
+      image: require('./assets/images/nextuLogo.webp'),
+      link: 'http://nextu.se/',
+    },
+  ],
+  display: false, // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji('Projects'),
+  subtitle:
+    'Projects and Some Cool Stuff that I have done !',
+
+  achievementsCards: [
+    {
+      title: 'E-Commerce Web Application',
+      subtitle:
+        'Developed an E-Commerce website built on React for the front-end and Node.js on the back-end.',
+      image: require('./assets/images/ecom3.png'),
+      footerLink: [
+        {
+          name: 'Website',
+          url:
+            'https://supernebr-a856a.firebaseapp.com/',
+        },
+      ],
+    },
+    {
+      title: 'Dataset Analysis',
+      subtitle:
+        'Preprocessed and analysed the dataset - Communities and Crime.',
+      image: require('./assets/images/data.png'),
+      footerLink: [
+        {
+          name: 'Github',
+          url:
+            'https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en',
+        },
+        {
+          name: 'Dataset',
+          url: 'http://archive.ics.uci.edu/ml/datasets/communities+and+crime'
+        }
+      ],
+    },
+
+    {
+      title: 'N-Queens Problem',
+      subtitle: 'Solving and Analysing the traditional N-Queens Problem using three AI algorithms.',
+      image: require('./assets/images/ai5.png'),
+      footerLink: [
+        {
+          name: 'Github',
+          url: 'https://github.com/sarvangjain/N-Queens',
+        },
+      ],
+    },
+  ],
+  display: true, // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: 'Blogs',
+  subtitle:
+    'With Love for Developing cool stuff, I love to write and teach others what I have learnt.',
+
+  blogs: [
+    {
+      url:
+        'https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae',
+      title: 'Win a Google Assistant Tshirt and $200 in Google Cloud Credits',
+      description:
+        'Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?',
+    },
+    {
+      url: 'https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e',
+      title: 'Why REACT is The Best?',
+      description:
+        'React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies.',
+    },
+  ],
+  display: true, // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: 'TALKS',
+  subtitle: emoji(
+    'I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅'
+  ),
+
+  talks: [
+    {
+      title: 'Build Actions For Google Assistant',
+      subtitle: 'Codelab at GDG DevFest Karachi 2019',
+      slides_url: 'https://bit.ly/saadpasta-slides',
+      event_url: 'https://www.facebook.com/events/2339906106275053/',
+    },
+  ],
+  display: true, // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji('Podcast 🎙️'),
+  subtitle: 'I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY',
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    'https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo',
+  ],
+  display: true, // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji('Contact Me ☎️'),
+  subtitle:
+    'Discuss a project or just want to say hi? My Inbox is open for all.',
+  number: '+91-8233828482',
+  email_address: 'sarvangjain@gmail.com',
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: 'twitter', //Replace "twitter" with your twitter username without @
+  display: true, // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+};
